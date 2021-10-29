203968879b6ae52d72ea01ebb2c7b1621672b263
parth-98 <75521208+parth-98@users.noreply.github.com>
2021-10-29 22:01:55 +0530

    api url update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const searchEl = document.getElementById("search");
 getCountries();
 
 async function getCountries() {
-  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const res = await fetch("https://restcountries.com/v2/all");
   const countries = await res.json();
   displayCountries(countries);
 }
